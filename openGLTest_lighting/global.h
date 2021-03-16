c13bc6cba9ec8fce4738d528c9cec35106fd84d5
Duckbobs <45709904+Duckbobs@users.noreply.github.com>
2021-03-16 09:53:53 +0900

    Clean Old Projects

--- openGLTest_lighting/global.h
+++
@@ -1,9 +0,0 @@
-#include <glm/vec3.hpp>
-#include <glm/vec4.hpp>
-#include <vector>
-#include <glm/glm.hpp>
-
-namespace global
-{
-	glm::vec3 computeNormal(glm::vec3 p1, glm::vec3 p2, glm::vec3 p3);
-};
--- openGLTest_lighting2/lighting.frag
+++
@@ -1,9 +0,0 @@
-#version 450 core
-
-in vec3 fLightIntensity;
-out vec4 FragColor;
-
-void main()
-{
-	FragColor = vec4(fLightIntensity, 1);
-} 
